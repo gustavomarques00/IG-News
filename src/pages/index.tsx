26c7ee6b8f77190af3a6b543c12a717e8610246c
Gustavo Marques <gustavomarquesmm@gmail.com>
2022-01-17 18:35:30 -0300

    Estruturação do projeto NEXT

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | IG News</title>
+      </Head>
+      <h1>
+        <p>oláaaa</p>
+      </h1>
+    </>
+  );
+}
