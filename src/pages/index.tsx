94e94a37e296513be1a9e30690a79e34f44f6cb6
Gustavo Marques <gustavomarquesmm@gmail.com>
2022-01-18 00:40:57 -0300

    Layout da home finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | IG News</title>
+        <title>Home | IG News</title>
       </Head>
-      <h1>
-        <p>oláaaa</p>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>News about the <span>React</span> World.</h1>
+          <p>Get acess to all the publications<br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton/>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   );
 }
