7197e56ab400367bf865fc331fb1bff13b79d9a3
Gustavo Marques <gustavomarquesmm@gmail.com>
2022-01-19 13:10:26 -0300

    Implementando o SSG (Static Side Generation)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 import Head from "next/head";
+import {GetStaticProps} from 'next'
 import { SubscribeButton } from "../components/SubscribeButton";
 import styles from './home.module.scss'
+import Stripe from "stripe";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number
+  }
+}
+
+export default function Home({product}) {
   return (
     <>
       <Head>
@@ -13,14 +23,34 @@ export default function Home() {
           <span>👏 Hey, Welcome</span>
           <h1>News about the <span>React</span> World.</h1>
           <p>Get acess to all the publications<br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton/>
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="girl coding" />
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps =  async () => {
+  const price = await stripe.prices.retrieve('price_1KJQ7oDZuf32mLwnwDpejMUk')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US',{
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product 
+    },
+    revalidate: 60*60*24 //24 Horas
+  }
+
 }
