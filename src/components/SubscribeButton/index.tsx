1d5f81c231703d6df2ab064b8ddafdcaa638f655
Gustavo Marques <gustavomarquesmm@gmail.com>
2022-03-29 14:12:55 -0300

    Configuração de redirecionamento para o Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,41 @@
-import styles from './styles.module.scss'
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJS } from "../../services/stripe-js";
+import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
-    priceId: string;
+  priceId: string;
 }
 
-export function SubscribeButton({priceId}:SubscribeButtonProps){
-    return(
-        <button type='button' className={styles.subscribeButton}>
-            Subscribe Now
-        </button>
-    )
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { status } = useSession();
+
+  async function handleSubscribe() {
+    if (status !== "authenticated") {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJS();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe Now
+    </button>
+  );
 }
